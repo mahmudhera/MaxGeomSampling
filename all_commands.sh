7e2eecce744b0fdca2322a0aaacf7920a51ee67b
Mahmudur <mahmudhera93@gmail.com>
2025-09-06 10:27:37 -0400

    add expt for MGS sample size against k

---
+++ all_commands.sh
@@ -0,0 +1,14 @@
+conda create -n maxgeom -y
+conda activate maxgeom
+pip install -r requirements.txt
+
+export PYTHONPATH=$(pwd):$PYTHONPATH
+
+# basic implementation test
+python scripts/expt_basic_implementation.py
+
+# growth against data size (against FMH)
+python scripts/expt_growth_of_samples_against_fmh.py > results/results_growth_of_samples
+
+# growth against k
+python scripts/expt_growth_of_mgs_varying_k.py > results/results_growth_of_mgs_varying_k
