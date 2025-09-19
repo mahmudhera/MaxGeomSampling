0a35d004217c2325f5b949988bc08ee3c3959d0b
Mahmudur <mahmudhera93@gmail.com>
2025-09-19 02:46:50 -0400

    run expt with another version

--- scripts/expt_accuracy_with_refining.py
+++ scripts/expt_accuracy_with_refining.py
@@ -5,8 +5,8 @@ from tqdm import tqdm
 
 if __name__ == '__main__':
     # constants
-    num_trails = 5000
-    num_samples_each_trial = 2
+    num_trails = 500
+    num_samples_each_trial = 1
     alpha_values_tested = [0.2, 0.3, 0.4, 0.5]
     k_values_tested = [50, 100, 200, 400]
     output_file = 'results/results_accuracy_of_jaccard_with_refining'
