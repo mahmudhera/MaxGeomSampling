0be5aa907c96e5d9225001a7c1d458919478a74b
Mahmudur Rahman Hera <mahmudhera93@gmail.com>
2025-11-07 00:24:07 -0500

    add MinHash implementation

---
+++ samplers/minhash/MinHash.py
@@ -0,0 +1,47 @@
+from hashes.hash_utils import get_mmh3_hash
+from typing import Iterable
+
+class MinHashSketch:
+    
+    def __init__(self, k: int, seed: int = 42):
+        self.k = k
+        self.seed = seed
+        self.all_items = set()
+        
+    def add_item(self, item: str):
+        self.all_items.add(item)
+            
+    def add_many_items(self, items: Iterable[str]):
+        for item in items:
+            self.add_item(item)
+
+
+    def create_minhash_sample(self):
+        # create k seeds based on self.seed
+        seeds = [self.seed + i for i in range(self.k)]
+        minhashes = []
+        for seed in seeds:
+            min_hash_this_seed = None 
+            for item in self.all_items:
+                hash_value = get_mmh3_hash(item, seed=seed)
+                if min_hash_this_seed is None or hash_value < min_hash_this_seed:
+                    min_hash_this_seed = hash_value
+            if min_hash_this_seed is not None:
+                minhashes.append(min_hash_this_seed)
+        self.minhashes = minhashes
+
+    
+    def __len__(self):
+        return self.k
+    
+
+    def jaccard_index(self, other: 'MinHashSketch') -> float:
+        num_mathes = 0
+        for minhash1, minhash2 in zip(self.minhashes, other.minhashes):
+            if minhash1 == minhash2:
+                num_mathes += 1
+        return num_mathes / self.k
+
+
+    def sample_size(self) -> int:
+        return self.k
