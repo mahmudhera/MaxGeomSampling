0af78f187cf36c37d701b22c796064200be49f95
Mahmudur Rahman Hera <mahmudhera93@gmail.com>
2025-11-07 00:24:19 -0500

    plot a-MGS sizes

--- scripts/plot_alpha_MGS_sample_sizes.py
+++ scripts/plot_alpha_MGS_sample_sizes.py
@@ -29,7 +29,7 @@ def plot_growth_against_multiple_alpha(data_filename, output_filename):
     # column names: set_size	amgs_sample_size_avg_alpha_0.25	amgs_sample_size_stddev_alpha_0.25	amgs_sample_size_avg_alpha_0.5	amgs_sample_size_stddev_alpha_0.5	amgs_sample_size_avg_alpha_0.75	amgs_sample_size_stddev_alpha_0.75
     # need to plot sample sizes for different k values against set_size with error bars as shaded region using stddev
 
-    alpha_values = [0.4, 0.5]
+    alpha_values = [0.3, 0.4, 0.5]
     colors = sns.color_palette("husl", len(alpha_values))
     for alpha, color in zip(alpha_values, colors):
         avg_col = f'amgs_sample_size_avg_alpha_{alpha}'
